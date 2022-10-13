6b17bff476ce0032ce79d048b796bfd098236aba
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-13 11:30:12 -0400

    finished homepage

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+const homeRouter = require('./home-routes.js');
+
 router.use('/api', apiRoutes);
+
+router.use('/', homeRouter);
 
 router.use((req, res) => {
   res.status(404).end();
