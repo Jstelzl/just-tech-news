0861e71030407a2a2861a9b9c7e867bf0ea7c15a
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-14 11:36:08 -0400

    created login page

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001
 
+const session = require('express-session');
+
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUnitialized: true,
+  store: new sequelizeStore({
+    db:sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
